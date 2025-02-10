153f8e60ae35740619700513e10a3016a6dc5a16
tc-yavuz <148723177+freekhadka@users.noreply.github.com>
2025-02-10 16:47:28 -0500

    fondation

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+/**
+* @type {import('vite').UserConfig}
+*/
+export default {
+  // Set the base directory for GitHub pages
+  base: '/tree-js/',
+  build: {
+    outDir: './dist',
+    sourcemap: true,
+  },
+  publicDir: './public',
+}
